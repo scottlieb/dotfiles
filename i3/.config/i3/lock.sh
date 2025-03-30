de86c5c371a44e20e71d2d103758062f666f6ef0
Amitai Gottlieb <amitaig@hailo.ai>
2025-03-30 10:40:07 +0300

    i3

---
+++ i3/.config/i3/lock.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+scrot /tmp/screenshot.png
+convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
+i3lock -i /tmp/screenshotblur.png
+rm /tmp/screenshot.png /tmp/screenshotblur.png
