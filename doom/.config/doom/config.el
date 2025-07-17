351d31a641dcef2c56b2e5a13a0a2b85a1092302
Amitai Gottlieb <amitaigottlieb@gmail.com>
2025-07-17 10:58:49 +0300

    more more doom

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -1,41 +1,5 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-;;(setq org-directory "~/org/")
-
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
 ;; ######
 ;; GLOBAL
 ;; ######
@@ -126,24 +90,23 @@
   ;; Use rsync to sync remote files; faster on large files.
   (setq tramp-default-method "rsync"))
 
-(defun string-at-selection (m p)
+;; ################
+;; CUSTOM FUNCTIONS
+;; ################
+
+(defun string-at-region (m p)
   (interactive "r")
-  (buffer-substring-no-properties m p)) ;
+  (buffer-substring-no-properties m p))
 
-(defun replace-selection (to-string)
-  (interactive "s")
-  (let ((from-string (call-interactively 'string-at-selection)))
-    (while (search-backward from-string 'nil t)
-        (replace-match to-string))
-    (while (search-forward from-string 'nil t)
-        (replace-match to-string))))
-
-;; ###########
-;; KEYBINDINGS
-;; ###########
+;; ############
+;; KEY-MAPPINGS
+;; ############
 
 (map! :desc "Switch between source/header file" "M-o" #'lsp-clangd-find-other-file)
 
 (map! :desc "Clear search highlight" "C-l" #'evil-ex-nohighlight)
 
-(map! :nv :desc "GoTo references" "g r" #'+lookup/references)
+(map! :map evil-motion-state-map
+  :desc "Go to References"      "gr" #'+lookup/references
+  :desc "Go to Definition"      "gd" #'+lookup/definition
+)
